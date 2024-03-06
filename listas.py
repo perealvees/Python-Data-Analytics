790181bf200e298edfcc32e623c516ceba14eb10
Priscilla Alves <perealvees@gmail.com>
2024-03-06 16:30:01 -0300

    listas criadas

---
+++ listas.py
@@ -0,0 +1,42 @@
+#append: adiciona elemento
+
+'''lista =[]
+
+lista.append(1)
+lista.append("Python")
+lista.append([41, 14, 90])
+
+print(lista, end=" ")'''
+
+#clear: retira elemento
+
+
+'''lista = [1, "Python", [41, 14, 90]]
+print(lista)
+
+lista.clear()
+print(lista)'''
+
+#copy: ele retorna todos os elementos com uma estância diferente
+
+'''lista = [1, "Python", [41, 14, 90]]
+
+l2 = lista.copy()
+print(lista)
+
+print(id(l2), id(lista))
+l2[0] = 2
+
+print(l2)
+print(lista)'''
+
+#count: retorna quantas vezes aquele objeto aparece na sua lista
+#pop: retira o ultimo elemento da lista: sendo que se você não passar nenhum valor, ele retira o primeiro da direita pra esquerda
+# mas você pode escolher a posição deseja, colocando o parametro dentro do (), mas nesse caso da esquerda pra direita.
+
+#remove: você escolhe o objeto, escrevendo dentro da string
+
+#sort: ordena a lista
+
+
+
